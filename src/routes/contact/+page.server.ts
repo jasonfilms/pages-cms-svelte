e05aa4d7589c3f04857c66c3f3540c963230528b
Rhys Kim <hello@rhyses-pieces.dev>
2024-12-31 11:59:03 -0500

    set up contact form

---
+++ src/routes/contact/+page.server.ts
@@ -0,0 +1,27 @@
+import type { Actions } from "@sveltejs/kit";
+import nodemailer from "nodemailer";
+import { MailtrapTransport } from "mailtrap";
+import { TOKEN, RECIPIENT } from "$env/static/private";
+
+export const actions = {
+  default: async ({ request }) => {
+    const data = await request.formData();
+    const transport = nodemailer.createTransport(MailtrapTransport({
+      token: TOKEN
+    }));
+    try {
+      transport.sendMail({
+      from: data.get("email") as string,
+      to: RECIPIENT,
+      subject: `Contact Form: ${data.get("name") as string}`,
+      html: `
+        <html>
+          <
+        </html>
+        `,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+} satisfies Actions;
