b2ab49947eaa2cb2c52b6b51b843b212cccf1ba6
Rhys Kim <hello@rhyses-pieces.dev>
2025-01-02 14:57:14 -0500

    remove unneeded deps and adjust nav + cms

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -7,8 +7,17 @@
 </script>
 
 <nav>
-  <button class="toggle" aria-label="Toggle menu" onclick={openMenu}>Menu</button>
-  <ul class={mobile ? 'menu' : ''}>
+  <button 
+    class="toggle" 
+    aria-expanded="{mobile}" 
+    aria-controls="main-menu" 
+    aria-label="Toggle menu" 
+    onclick={openMenu}
+  >
+    Menu
+  </button>
+  <ul id="main-menu" class={mobile ? "menu" : ""}>
+    <button class={["close", mobile ? "show" : ""]} aria-label="Close menu" onclick={() => mobile = false}>x</button>
     <li><a href="/">index</a></li>
     <li><a href="/about">about</a></li>
     <li><a href="/contact">contact</a></li>
@@ -21,7 +30,7 @@
     flex-wrap: wrap;
     font-family: var(--title-font);
 
-    .toggle {
+    .toggle, .close {
       display: none;
     }
 
@@ -32,8 +41,8 @@
       gap: 1rem;
     }
 
-    @media only screen and (max-width: 1180px) {
-      .toggle {
+    @media screen and (max-width: 1180px) {
+      .toggle, .show {
         display: block;
       }
 
@@ -42,7 +51,14 @@
       }
 
       .menu {
+        position: fixed;
+        z-index: 99;
         display: block;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: var(--background);
       }
     }
   }
